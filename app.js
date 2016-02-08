427d780046c53c24726410c8d99d2e4bbbaa6c1c
Paula Lehtola <pklehtol@cs.helsinki.fi>
2016-02-08 21:23:09 +0200

    bootstrap and controllers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = angular.module('imagez', []);
 
+
+
 app.factory('images', [function() {
   var o = { images: [] };
   return o;
@@ -9,28 +11,35 @@ app.controller('MainCtrl', [
 '$scope', 'images',
 function($scope, images) {
   $scope.images = images.images;
-  $scope.image = {
-    path: "",
-    width: 0,
-    height: 0
-  }
+
   $scope.loadimage = function() {
     var img = new Image();
-    img.onload = function() {
+    
+    img.src = $scope.imageurl;
+    $scope.images.push(img);
+  }
+}]);
+
+app.controller('ImageCtrl', [
+ '$scope', 'images',
+  function($scope, images, $http, $routeParams) {
+    $scope.images = images.images;
+    $scope.image = {
+      path: "",
+      width: 0,
+      height: 0
+    }
+    $scope.enlargeimage = function(img) {
+ 
+      img.onload = function() {
       $scope.$apply(function() {
         $scope.image.width = img.width;
         $scope.image.height = img.height;
         $scope.image.path = $scope.imageurl;
        
       });
+      img.src = $scope.imageurl;
     }
-    
-    img.src = $scope.imageurl;
-    $scope.images.push(img);
-  }
-
-  $scope.enlargeimage = function(img) {
-    
-  }
+    }
 
 }]);
